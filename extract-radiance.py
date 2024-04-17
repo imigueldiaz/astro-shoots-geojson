2c30a3cd71bff7e5792575f5653b2294988b91c0
Ignacio de Miguel Diíaz <imigueldiaz@gmail.com>
2024-04-17 09:48:55 +0200

    Add console theming and extraction progress tracking

--- extract-radiance.py
+++ extract-radiance.py
@@ -5,6 +5,25 @@ import gzip
 import shutil
 import zipfile
 from countries_data import COUNTRIES_DATA
+from rich.console import Console
+from rich.theme import Theme
+from rich.progress import Progress
+import os
+import locale
+
+# Set the locale to the default system locale
+locale.setlocale(locale.LC_ALL, '')
+
+# Define a custom theme for the console
+custom_theme = Theme({
+    'info': 'green',
+    'warning': 'yellow',
+    'error': 'bold red',
+    'progress': 'blue'
+})
+
+# Create a console object with the custom theme
+console = Console(theme=custom_theme)
 
 # Function to convert radiance to Bortle scale with 0.1 precision
 def mpsasToBortle(mpsas):
@@ -26,6 +45,13 @@ def mpsasToBortle(mpsas):
             bortle = bortle_low + (mpsas - mpsas_low) * (bortle_high - bortle_low) / (mpsas_high - mpsas_low)
             return round(bortle, 1)
 
+# Convert radiance to magnitudes per square arcsecond
+# This formula assumes the radiance is measured in the V band (visual magnitude) with a wavelength around 550 nm. 
+# The constant 20.7233 is derived from the definition that a surface brightness of 0 mpsas
+# corresponds to a radiance of 4.0 x 10^-8 W/cm2/sr in the V band.
+def radianceToMpsas(radiance):
+    return -2.5 * np.log10(radiance) + 20.7233
+
 # Function to export the extracted data to a CSV file
 def export_csv(data, filename):
 
@@ -35,12 +61,8 @@ def export_csv(data, filename):
             latitude = row[0]
             longitude = row[1]
             radiance = row[2]
-
-            # Convert radiance to magnitudes per square arcsecond
-            # This formula assumes the radiance is measured in the V band (visual magnitude) with a wavelength around 550 nm. 
-            # The constant 20.7233 is derived from the definition that a surface brightness of 0 mpsas
-            # corresponds to a radiance of 4.0 x 10^-8 W/cm2/sr in the V band.
-            mpsas = -2.5 * np.log10(radiance) + 20.7233
+            
+            mpsas = radianceToMpsas(radiance)
 
             # Convert mpsas to Bortle scale on a homemade continuous scale with 0.1 precision, 
             # simply to have a better understanding of the light pollution level.
@@ -59,12 +81,76 @@ def zip_file(filename, zip_filename, verbose):
         zipf.write(filename)
     log(f"File compressed to {zip_filename}", verbose)
 
+# Function to log an info message
 def log(message, verbose):
     if verbose:
-        print(message)
+        console.print(f"[info]INFO:[/info] {message}")
+
+# Function to log an error message and exit the program
 def error(message):
-    print(message)
+    console.print(f"[error]ERROR:[/error] {message}")
     exit(1)
+
+#Function to fotmat the number with the user locale
+def format_number(number):
+    return locale.format_string("%.f", number, grouping=True)
+
+# Function to log the size and format of the exported data
+def log_export_data(format, size):
+    return(f"Exporting data to {format} file with {format_number(size)} recorded coordinates.")
+
+# Function to ask the user if they want to extract data for the whole Spain or for specific regions
+def process_spain_regions():
+    console.print("Do you want to extract data for the whole Spain or for specific regions?")
+    console.print("1. Whole Spain")
+    console.print("2. Specific regions")
+    choice = console.input("Enter your choice (1/2): ")
+    if choice == "1":
+        return COUNTRIES_DATA["ESP"]
+    elif choice == "2":
+        console.print("Select the region you want to extract data for:")
+        console.print("1. Canary Islands")
+        console.print("2. Balearic Islands")
+        console.print("3. Spanish Peninsula")
+        region_choice = console.input("Enter your choice (1/2/3): ")
+        if region_choice == "1":
+            return COUNTRIES_DATA["ESP_CANARY"]
+        elif region_choice == "2":
+            return COUNTRIES_DATA["ESP_BALEARIC"]
+        elif region_choice == "3":
+            return COUNTRIES_DATA["ESP_PENINSULA"]
+        else:
+            error("Error: Invalid region choice.")
+            return None
+    else:
+        error("Error: Invalid choice.")
+        return None
+
+# Function to extract data from the raster file
+def process_range_data(raster, min_row, max_row, min_col, max_col, sampling_interval, origin_x, origin_y, pixel_width, pixel_height):
+    total_iterations = ((max_row - min_row + 1) // sampling_interval) * ((max_col - min_col + 1) // sampling_interval)
+    range_data = []
+
+    with Progress() as progress:
+        task = progress.add_task("[progress]Extracting data...", total=total_iterations)
+
+        for i in range(min_row, max_row + 1, sampling_interval):
+            for j in range(min_col, max_col + 1, sampling_interval):
+                # Calculate the geographic coordinates of the current pixel
+                x = origin_x + j * pixel_width
+                y = origin_y + i * pixel_height
+                
+                # Read the light pollution value at the current pixel
+                data = raster.ReadAsArray(j, i, 1, 1)
+                light_pollution = data[0, 0]
+                
+                # Append the data to the list if light pollution is not zero
+                if light_pollution > 0.0:
+                    range_data.append([y, x, light_pollution])
+                
+                progress.update(task, advance=1)
+
+    return range_data
 
 
 # Main function to extract radiance data from a raster file and export it to a CSV file
@@ -92,14 +178,19 @@ def main():
 
     args = parser.parse_args()
 
+    #if the args.input_file file does not exist, return an error.
+    if not os.path.exists(args.input_file):
+        error("The input file does not exist.")
+        return
+
     if(args.sampling_interval < 0.5):
-        error("Error: Sampling interval must be greater or equal than 0.5km. The GeoTIFF image has 15 arcseconds for each pixel")
+        error("Sampling interval must be greater or equal than 0.5km. The GeoTIFF image has 15 arcseconds for each pixel")
         return
 
     raster = gdal.Open(args.input_file, gdal.OF_RASTER)
 
     if raster is None:
-        error("Error: Could not open the raster file.", args.verbose)
+        error("Could not open the raster file.", args.verbose)
     else:
         log("Raster file opened successfully.", args.verbose)
     
@@ -113,7 +204,7 @@ def main():
         # Get the number of rows and columns in the raster
         cols = raster.RasterXSize # Number of columns
         rows = raster.RasterYSize # Number of rows
-        log(f"Number of rows: {rows}, Number of columns: {cols}", args.verbose)
+        log(f"Number of rows: {rows:n}, Number of columns: {cols:n}", args.verbose)
         
 
         if args.country:
@@ -125,29 +216,8 @@ def main():
                 return
             # If args.country is ESP we ask to the user if they want to extract data for the whole Spain or for specific regions
             if args.country == "ESP":
-                print("Do you want to extract data for the whole Spain or for specific regions?")
-                print("1. Whole Spain")
-                print("2. Specific regions")
-                choice = input("Enter your choice (1/2): ")
-                if choice == "1":
-                    country_data = COUNTRIES_DATA["ESP"]
-                elif choice == "2":
-                    print("Select the region you want to extract data for:")
-                    print("1. Canary Islands")
-                    print("2. Balearic Islands")
-                    print("3. Spanish Peninsula")
-                    region_choice = input("Enter your choice (1/2/3): ")
-                    if region_choice == "1":
-                        country_data = COUNTRIES_DATA["ESP_CANARY"]
-                    elif region_choice == "2":
-                        country_data = COUNTRIES_DATA["ESP_BALEARIC"]
-                    elif region_choice == "3":
-                        country_data = COUNTRIES_DATA["ESP_PENINSULA"]
-                    else:
-                        error("Error: Invalid region choice.")
-                        return
-                else:
-                    error("Error: Invalid choice.")
+                country_data = process_spain_regions()
+                if not country_data:
                     return
 
             if country_data:
@@ -158,7 +228,7 @@ def main():
                 log(f"Extracting data for {country_data['Name']} with bounding box: {min_lat} - {max_lat} (lat), {min_lon} - {max_lon} (lon)", args.verbose)
 
             else:
-                error("Error: Could not find the country data.")
+                error("Could not find the country data.")
                 return
 
         else:
@@ -167,7 +237,7 @@ def main():
 
             # if some of the bounding box values are not provided, use the values from the countries_data.py file
             if not min_lat or not max_lat or not min_lon or not max_lon:
-                error("Error: Bounding box values not provided. Did you forget to provide the --country argument?")
+                error("Bounding box values not provided. Did you forget to provide the --country argument?")
                 return
         
         # Calculate the pixel indices for Spain's bounding box
@@ -183,39 +253,27 @@ def main():
         #The region name should be in the country_data dictionary or in the passed arguments
         region_name = country_data.get("Name") if country_data else "Custom region"
 
-        log(f"Sampling interval: {sampling_interval}px for {args.sampling_interval}km in {region_name}.\n {km_to_arcseconds:.3f} arcseconds for {args.sampling_interval}km", args.verbose)
+        log(f"Sampling interval: {sampling_interval}px for {args.sampling_interval:.2f}km in {region_name}, {km_to_arcseconds:.3f} arcseconds for the interval", args.verbose)
 
-        # Create a list to store the extracted data for Spain
-        range_data = []
-        
-        # Iterate over the pixels within Spain's bounding box
-        for i in range(min_row, max_row + 1, sampling_interval):
-            for j in range(min_col, max_col + 1, sampling_interval):
-                # Calculate the geographic coordinates of the current pixel
-                x = origin_x + j * pixel_width
-                y = origin_y + i * pixel_height
-                
-                # Read the light pollution value at the current pixel
-                data = raster.ReadAsArray(j, i, 1, 1)
-                light_pollution = data[0, 0]
-                
-                # Append the data to the list if light pollution is not zero
-                if light_pollution > 0.0:
-                    range_data.append([y, x, light_pollution])
+        # Create a list to store the extracted data. Each element is a list with the latitude, longitude, and radiance values
+        range_data = process_range_data(raster, min_row, max_row, min_col, max_col, sampling_interval, origin_x, origin_y, pixel_width, pixel_height)
         
         #Export the extracted data to a CSV file
+        size = len(range_data)
+
         if(args.output_format == "CSV"):
-            log(f"Exporting data to CSV file with {len(range_data)} recorded coordinates.", args.verbose)
-            export_csv(range_data, args.output_file)
+            with console.status(log_export_data("CSV", size)):
+                export_csv(range_data, args.output_file)
+        
         log("Data extraction and export completed successfully.", args.verbose)
 
         #gzip the file
         if(args.gzip):
-            log("Compressing the file with gzip...", args.verbose)
-            gzip_file(args.output_file, f"{args.output_file}.gz", args.verbose)
+            with console.status("Compressing the file with gzip..."):
+                gzip_file(args.output_file, f"{args.output_file}.gz", args.verbose)
         if(args.zip):
-            log("Compressing the file with zip...", args.verbose)
-            zip_file(args.output_file, f"{args.output_file}.zip", args.verbose)
+            with console.status("Compressing the file with zip..."):
+                zip_file(args.output_file, f"{args.output_file}.zip", args.verbose)
 
 if __name__ == '__main__':
     main()
