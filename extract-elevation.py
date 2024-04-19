fb20c572e22d4e3b9ad651bcc738c18a825eab9a
Ignacio de Miguel Diíaz <imigueldiaz@gmail.com>
2024-04-19 12:29:01 +0200

    Add functions to download, unzip, extract & export elevation

--- extract-elevation.py
+++ extract-elevation.py
@@ -17,7 +17,7 @@ import tempfile
 from http import cookiejar
 from urllib import request
 from urllib.parse import urlencode
-
+from osgeo import gdal
 
 load_dotenv()
 
@@ -89,36 +89,80 @@ def process_spain_regions():
         console.print("[bold red]Error: Invalid choice.[/bold red]")
         return None
 
-def download_url(url, save_path, chunk_size=8192):
+# Function to download a file from a URL and save it to a local path
+# It uses NASA_BEARER to authenticate the request
+def download_url(url, save_path, verbose):
     headers = {"Authorization": f"Bearer {NASA_BEARER}"}
     try:
         r = requests.get(url, stream=True, allow_redirects=True, headers=headers)
         r.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
         with open(save_path, 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=chunk_size):
+            for chunk in r.iter_content(chunk_size=8192):
                 fd.write(chunk)
         return True  # Return True to indicate successful download
     except requests.exceptions.RequestException as e:
-        print(f"Error downloading {url}: {e} skipping...")
+        log(f"Error downloading {url}: {e} skipping...",verbose)
         return True  # Return True to indicate failed download but
 
+# Function to unzip a file and extract the .hgts files
+def unzip_file(zip_file, verbose):
+    extracted_hgts = []
+    
+    if not os.path.exists(zip_file):
+        return extracted_hgts
+    
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        listOfFileNames = zip_ref.namelist()
+        for fileName in listOfFileNames:
+            log(f"Unziping´{zip_file}", verbose)
+            if fileName.endswith('.hgts'):
+                zip_ref.extract(fileName, os.path.dirname(zip_file))
+                log(f"Extracted {fileName} from {zip_file}", verbose)
+                extracted_hgts.append(os.path.join(os.path.dirname(zip_file), fileName))
+                break
 
+            else:
+                pass
+    return extracted_hgts
+
+# Function to obtain the elevation data from the .hgts files
+def obtain_elevation(hgts_files, verbose):
+    total_lon, total_lat, total_elevations = [], [], []
+    for hgts_file in hgts_files:
+        file = hgts_file[0]
+        
+        # Extract latitude and longitude from the filename
+        lat = int(file[-11:-9])
+        lon = int(file[-8:-5])
+        if file[-12] == 's':
+            lat = -lat
+        if file[-9] == 'w':
+            lon = -lon
+        
+        raster = gdal.Open(file)
+        elevations = raster.ReadAsArray()
+        elevations[elevations == -32768] = np.nan
+        
+        # Generate latitude and longitude values for this specific file
+        lat_vals = np.arange(lat + 1, lat, -0.00277777777777)
+        lon_vals = np.arange(lon, lon + 1, 0.00277777777777)
+        lats, lons = np.meshgrid(lat_vals, lon_vals)
+        
+        total_lat.append(lats)
+        total_lon.append(lons)
+        total_elevations.append(elevations)
+
+    log(f"Extracted elevation data from {len(hgts_files)} files", verbose)
+    return total_lat, total_lon, total_elevations
 
 def extract_elevation(min_lat, max_lat, min_lon, max_lon, verbose):
-   
-    # Create a progress bar to show the download progress
-    #progress = Progress(f"[progress]Downloading elevation data for the coordinates {min_lat} {min_lon} - {max_lat} {max_lon} [/progress]", "{task.completed}/{task.total}")
-
-    # Define the task for the progress bar
-    #task = progress.add_task("[progress]Downloading...[/progress]", total=rows)
-
-    # Start the progress bar
-    #progress.start()
-
-    temp_dir = "temp"
+    
+    
+    #create a temp dir from the program path
+    temp_dir = os.path.join(os.path.dirname(__file__), "temp")
     os.makedirs(temp_dir, exist_ok=True)
 
-
+    hgts_files = []
     # The NASADEM dataset is stored in 1-degree tiles, so we need to download multiple tiles to cover the bounding box
     # The filename format is NASADEM_SHHP_{n or s}{latitude 2 digits zero padded}{e or w}{longitude 2 digits zero padded}.zip
     # https://e4ftl01.cr.usgs.gov/MEASURES/NASADEM_SHHP.001/2000.02.11/NASADEM_SHHP_n42e000.zip
@@ -136,24 +180,93 @@ def extract_elevation(min_lat, max_lat, min_lon, max_lon, verbose):
             # Define the URL to download the tile
             url = f"{NASA_URL}{filename}"
 
+            #define the path to save the file
+            save_path = os.path.join(temp_dir, filename)
+
             # Download the tile from the NASADEM dataset
             # If the file does not exist, skip to the next tile
-            if not os.path.exists(filename):
+            if not os.path.exists(save_path):
                 log(f"Downloading tile {filename} from {url}", verbose)
                 try:
-                    download_url(url, f"{temp_dir}/{filename}")
+                    download_url(url, save_path, verbose)
                     log(f"Downloaded tile {filename}", verbose)
                 except Exception as e:
                     error(f"Error downloading tile {filename}: {e}")
+            else:
+                log(f"Tile {filename} already exists, skipping download", verbose)
 
-    # Finish the progress bar
-    #progress.stop()
+            #From the zip we extract the .hgt file
+            extracted_hgts = unzip_file(save_path, verbose)
+            if extracted_hgts:
+                hgts_files.append(extracted_hgts)
+            else:
+                log(f"No .hgts files found in {save_path}, skipping...", verbose)
+                continue
 
-    return None
+            # If the tile is downloaded, extract the elevation data from the tile
+
+    return obtain_elevation(hgts_files, verbose)
+
+# Function to export the extracted data to a GeoJSON file
+def export_geojson(outputfile, global_lat, global_lon, global_elevations, verbose):
+    features = []
+    for lats, lons, elevations in zip(global_lat, global_lon, global_elevations):
+        for row in range(lats.shape[0]):  # use shape instead of len
+            for col in range(lats.shape[1]):
+                lat = lats[row][col]
+                lon = lons[row][col]
+                alt = elevations[row][col]
+
+                # Skip points with no elevation data
+                if np.isnan(alt):
+                    continue
+
+                feature = {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [float(lon), float(lat), float(alt)]
+                    }
+                }
+                features.append(feature)
+
+    # Build final GeoJSON FeatureCollection
+    geojson = {"type": "FeatureCollection", "features": features}
+
+    log(f"Generated {len(features)} coordinates", verbose)
+
+    with open(f"{outputfile}.json", "w") as f:
+        json.dump(geojson, f)
+
+    log(f"Extracted data written to {outputfile}.json", verbose)
+
+
+
+
+# Function to export the extracted data to a CSV file
+def export_csv(outputfile, global_lat, global_lon, global_elevations, verbose):
+    with open(outputfile, 'w') as f:
+        f.write("latitude,longitude,elevation\n")
+        for row in range(len(elevations)):
+            for col in range(len(elevations[0])):
+                lat = lats[row][col]
+                lon = lons[row][col] 
+                alt = elevations[row][col]
+                
+                # Skip points with no elevation data
+                if np.isnan(alt):
+                    continue
+                
+                f.write(f"{lat},{lon},{alt}\n")
+        
+    log(f"[info]Extracted data written to {args.output}.csv[/info]", verbose)
 
 def main():
+
+    global_lat, global_lon, global_elevations = [], [], []
+
     parser = argparse.ArgumentParser(description='Extract elevation data from given coordinates')
-    parser.add_argument('--output', type=str, help='The output filename to write the extracted data to')
+    parser.add_argument('--output', type=str, help='The output filename to write the extracted data to', default='elevation');
     parser.add_argument('--format', type=str, choices=['json', 'csv'], default='json', help='The format to write the extracted data in (default: json)')
     parser.add_argument('--minlat', type=float, help='Minimum latitude of the bounding box')
     parser.add_argument('--maxlat', type=float, help='Maximum latitude of the bounding box')
@@ -205,7 +318,6 @@ def main():
             else:
                 error("Could not find the country data.")
                 return
-
     else:
         # Define the bounding box from the arguments
         min_lat, max_lat,min_lon, max_lon = args.minlat, args.maxlat, args.minlon, args.maxlon
@@ -216,7 +328,14 @@ def main():
             return
 
     # Extract the elevation data from the input file
-    elevation_data = extract_elevation(min_lat, max_lat, min_lon, max_lon, args.verbose)
+    global_lat, global_lon, global_elevations = extract_elevation(min_lat, max_lat, min_lon, max_lon, args.verbose)
+
+    # Export the extracted data to the output file
+    if args.output:
+        if args.format == "json":
+            export_geojson(args.output, global_lat, global_lon, global_elevations, args.verbose)
+        elif args.format == "csv":
+            export_csv(args.output, global_lat, global_lon, global_elevations,args.verbose)
 
 
 if __name__ == '__main__':
